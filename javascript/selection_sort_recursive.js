2ccc15a3d782616aab0d0e8862dccf1484eac157
Keyvan Alonso <keyvanalonso@gmail.com>
2022-06-01 00:39:57 +0200

    completed lab

--- javascript/selection_sort_recursive.js
+++ javascript/selection_sort_recursive.js
@@ -1,12 +1,23 @@
 function selectionSortRecursive(arr) {
-  // type your code here
+  //console.log(arr)
+  if (arr[0] == undefined){
+    return arr
+  }
+  let min = Math.min(...arr)
+  let indx = arr.indexOf(min)
+  arr.splice(indx,1)
+  selectionSortRecursive(arr)
+  arr.unshift(min)
+  return arr
 }
 
 if (require.main === module) {
   // add your own tests in here
   console.log("Expecting: [-1, 2, 3, 5]");
   console.log("=>", selectionSortRecursive([3, -1, 5, 2]));
-
+  console.log("");
+  console.log("Expecting: [1, 2, 3, 4, 5, 6, 7]");
+  console.log("=>", selectionSortRecursive([3, 4, 1, 7, 6, 5, 2]));
   console.log("");
 }
 
